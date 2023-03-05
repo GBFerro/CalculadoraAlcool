8a3910a4a52a13ecb0e6271b53fff6381727f416
GBFerro <giovani.ferro@usp.br>
2023-03-05 18:27:55 -0300

    Add project files.

---
+++ CalculaAlcool/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float pAlcool, pGasolina;
+
+        Console.WriteLine("Digite o preço do álcool: ");
+        pAlcool = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o preço da gasolina: ");
+        pGasolina = float.Parse(Console.ReadLine());
+
+        if ((pAlcool/pGasolina) < 0.72) 
+        {
+            Console.WriteLine("O álcool esta mais em conta");
+        } else { 
+            Console.WriteLine("A gasolina está mais em conta"); 
+        }
+    }
+}
